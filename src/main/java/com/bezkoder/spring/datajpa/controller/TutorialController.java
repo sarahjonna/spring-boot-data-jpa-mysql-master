c004de149ced6fb2fda1fa8fd5dab024e7eefd5c
sarahjonna <100998930+sarahjonna@users.noreply.github.com>
2024-04-01 23:14:49 +0530

    sigin and jwt auth

--- src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.datajpa.model.Tutorial;
 import com.bezkoder.spring.datajpa.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:9000")
+@CrossOrigin(origins = "http://localhost:9000", maxAge = 3600, allowCredentials="true")
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
